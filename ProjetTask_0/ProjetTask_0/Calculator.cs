6e0e302c6cda8927aadf9918960257440eee9ff9
Auraen <aurane.martin@etu.univ-tours.fr>
2022-03-22 16:17:02 +0100

    First Class and Unit Tests

---
+++ ProjetTask_0/ProjetTask_0/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetTask_0
+{
+    public class Calculator
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ ProjetTask_0/ProjetTask_0Tests/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetTask_0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetTask_0.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void should_return_same_value()
+        {
+            int expected = 5;
+            int actual = Calculator.Addition(2, 3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void should_return_different_value()
+        {
+            int expected = 9;
+            int actual = Calculator.Addition(2, 3);
+
+            Assert.AreNotEqual(expected, actual);
+        }
+    }
+}
